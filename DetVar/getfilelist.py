fceb956a3c20fa1635a074eef48733fdde5f0daa
Wenqiang Gu <wgu@bnl.gov>
2020-08-18 23:35:21 -0500

    update script to get file list

---
+++ DetVar/getfilelist.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import os
+
+with open('DetVar.smp') as f:
+  for line in f:
+    c = line.split()
+    if len(c) != 2: continue
+    cmd = './getFilelistByDefName.sh %s > %s.txt' %(c[1], c[0])
+    print(cmd)
+    os.system(cmd)
