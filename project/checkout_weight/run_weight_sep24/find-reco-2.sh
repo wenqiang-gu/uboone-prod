46fe1dee66f65a71be9ee0405aa9aa2c050fd2e2
Wenqiang Gu <wgu@bnl.gov>
2020-10-07 12:33:43 -0500

    add scripts for new weights

---
+++ project/checkout_weight/run_weight_sep24/find-reco-2.sh
@@ -0,0 +1,5 @@
+# find /pnfs/uboone/scratch/users/wgu/WCP-001404/prodgenie_bnb_nu_overlay_run1* -size +1M -name "Physics*root" -o -name "Beam*root"  > prodgenie_bnb_nu_overlay_run1-reco2.txt
+# find /pnfs/uboone/scratch/users/xji/WCP-001404/prodgenie_bnb_nu_overlay_run1* -size +1M -name "Physics*root" -o -name "Beam*root" >> prodgenie_bnb_nu_overlay_run1-reco2.txt
+
+find /pnfs/uboone/scratch/users/wgu/WCP-001404/prodgenie_bnb_nu_overlay_run3* -size +1M -name "Physics*root" -o -name "Beam*root"  > prodgenie_bnb_nu_overlay_run3-reco2.txt
+find /pnfs/uboone/scratch/users/xji/WCP-001404/prodgenie_bnb_nu_overlay_run3* -size +1M -name "Physics*root" -o -name "Beam*root" >> prodgenie_bnb_nu_overlay_run3-reco2.txt
