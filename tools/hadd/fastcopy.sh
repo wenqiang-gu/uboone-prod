3bfb83e6db959f5a2d2e3dffeb8d9cfd4e65eb1d
Wenqiang Gu <wgu@bnl.gov>
2022-03-08 15:42:09 -0600

    add local dir

--- tools/hadd/fastcopy.sh
+++ tools/hadd/fastcopy.sh
@@ -1,8 +1,19 @@
 # echo $1
 
+INPUT_FILE=$1
+LOCAL_DIR=$2
+if [ -z "$LOCAL_DIR" ]
+then
+  echo "local dir: None -> set to files/"
+  LOCAL_DIR="files/"
+fi
+
+echo "local dir: " $LOCAL_DIR 
+mkdir $LOCAL_DIR
+
 count=0
 for file in `cat $1`;do
-  timeout 3s ifdh cp $file files/ 
+  timeout 3s ifdh cp $file $LOCAL_DIR
   echo $count $file
   let count++
 done
